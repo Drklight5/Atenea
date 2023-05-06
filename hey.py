474dd2ce942fdf76e8713a03331b67eb1f25eb37
Valeria <valeria050603@hotmail.com>
2023-05-06 14:12:52 -0600

    first commit

---
+++ hey.py
@@ -0,0 +1,7 @@
+import pandas as pd #Analisis y limpieza de datos
+import numpy as nu
+import matplotlib.pyplot as mt
+
+data = pd.read_csv("dataset_transacc_tdc_tdd_20230309.csv")
+print(data.head())
+print("H")
