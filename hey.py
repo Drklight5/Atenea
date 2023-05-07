416f678aebce0bb8692d8fd30d86f81090948cb9
Valeria <valeria050603@hotmail.com>
2023-05-07 01:39:16 -0600

    creación de la pagina

--- hey.py
+++ hey.py
@@ -3,5 +3,10 @@ import numpy as nu
 import matplotlib.pyplot as mt
 
 data = pd.read_csv("dataset_transacc_tdc_tdd_20230309.csv")
-print(data.head())
-print("H")
+
+
+f_edad = (data["edad_cliente"]>12) & (data["edad_cliente"]<100)
+data = data[(data["edad_cliente"]>12) & (data["edad_cliente"]<100)]
+
+frecuencia_giro= data['giro_nombre'].value_counts()
+frecuencia_giro.plot.bar(rot=90, title="")
